2cc1c4170cfb1aad158b2ccf38eb036b45f14c26
Carlos Pretelin <carlosppretelin@gmail.com>
2022-12-12 23:17:35 -0600

    very basic footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import "../styles/Footer.scss"
+import ProfilePic from "../assets/CarlosPic.png"
 
 export const Footer = () => {
   return (
-    <div className='Footer'>
-      <h1>Carlos Pretelin</h1>
-    </div>
+    <section className='Footer'>
+
+
+        <div className='Footer-Profile'>
+          <img src={ProfilePic} alt="Carlos Pretelin" />
+
+          <div>
+            <h3>Carlos Pretelin <br /> Fronted Developer</h3>
+          </div>
+          
+        </div>
+
+
+        <div className='Footer-ContactInfo'>
+          <div>
+            <i  className="fa-brands fa-linkedin  Footer-ContactInfo-item"></i>
+            <a target="_blank" href="https://www.linkedin.com/in/carlos-pretelin/">Linkedin</a>
+          </div>
+
+          <div>
+            <i className="fa-brands fa-github Footer-ContactInfo-item"></i>
+            <a target="_blank" href="https://github.com/Carlos-Pretelin"> GitHub</a>
+          </div>
+
+          <div>
+            <i className="fa-brands fa-twitter Footer-ContactInfo-item"></i>
+            <a target="_blank" href="https://www.linkedin.com/in/carlos-pretelin/">@CarlosPretelin</a>
+          </div>
+
+          <div>
+            <i className="fa-brands fa-google Footer-ContactInfo-item"></i>
+            <a target="_blank" href="mailto:carlosppretelin@gmail.com">carlosppretelin@gmail.com</a>
+          </div>
+
+          
+        </div>
+    </section>
   )
 }
